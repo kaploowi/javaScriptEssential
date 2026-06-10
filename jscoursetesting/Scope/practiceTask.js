af9e5ad22546eb8392d27ca7bbd6e71edf436b26
nathan <nathanielbn18@gmail.com>
2026-06-10 03:51:25 -0400

    more message

---
+++ jscoursetesting/Scope/practiceTask.js
@@ -0,0 +1,17 @@
+{
+    let a = 1;
+    const pi = 3.14;
+    var b = 2;
+
+    console.log(a);
+    console.log(pi);
+    console.log(b);
+}
+
+a = 10;
+pi = 4;
+b = 20;
+
+console.log(a);
+console.log(pi);
+console.log(b);
